47368daef170f7731b59df033a93d6134a27ed8d
EduardoAlvim <105020093+EduardoAlvim@users.noreply.github.com>
2022-05-17 14:13:19 -0300

    Update ex2 lista2.c

--- ex2 lista2.c
+++ ex2 lista2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*O  custo  ao  consumidor  de  um  carro  novo  �  a  soma  do  custo  de  f�brica  com  a percentagem do distribuidor e dos impostos (aplicados ao custo de f�brica).
-Supondo que a percentagem do distribuidor seja de 28% e os impostos de 45%,
-escrever um algoritmo que leia o custo de f�brica de um carro e escreva o custo ao consumidor. */
+/*O  custo  ao  consumidor  de  um  carro  novo  é  a  soma  do  custo  de  fábrica  com  a percentagem do distribuidor e dos impostos (aplicados ao custo de fábrica).
+Supondo que a percentagem do distribuidor seja de 28% e os impostos de 45%, faça uma função que leia o custo de fábrica de um carro 
+e retorne função principal o custo ao consumidor.  */
 
 float calculo();
 
