61ae55c20cfe34b85ac958419cb9378e177b522b
EduardoAlvim <105020093+EduardoAlvim@users.noreply.github.com>
2022-05-17 14:12:23 -0300

    Add files via upload

---
+++ ex2 lista2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*O  custo  ao  consumidor  de  um  carro  novo  é  a  soma  do  custo  de  fábrica  com  a percentagem do distribuidor e dos impostos (aplicados ao custo de fábrica).
+Supondo que a percentagem do distribuidor seja de 28% e os impostos de 45%,
+escrever um algoritmo que leia o custo de fábrica de um carro e escreva o custo ao consumidor. */
+
+float calculo();
+
+void main()
+{
+    float r;
+    r=calculo();
+    printf("\nO custo de seu carro sera: %.2f",r);
+}
+
+float calculo(){
+    float custof, custo, pdd, imp;
+    printf("\nDigite o custo de fabrica do carro: ");
+    scanf("%f",&custof);
+    pdd = custof*0.28;
+    imp = custof*0.45;
+    custo = custof + pdd + imp;
+    return custo;
+}
